c681eba69f6bcd74a88ee137beb6b09735f24081
camerondrennan <camerondrennan@live.co.uk>
2015-11-17 14:55:55 +0000

    Got the bluetooth detecting R2 and L2 buttons, unable to test motors

--- src/PS3BT/PS3BT.ino
+++ src/PS3BT/PS3BT.ino
@@ -1,6 +1,7 @@
 #include <PS3BT.h>
-#include <usbhub.h>
 #include <Servo.h>
+#include <usbhub.h>
+
 
 // Satisfy the IDE, which needs to see the include statment in the ino too.
 #ifdef dobogusinclude
@@ -9,52 +10,47 @@
 #endif
 
 USB Usb;
-USBHub Hub1(&Usb); // Some dongles have a hub inside
-
-Servo myMotor;
-Servo myMotor2;
-Servo myMotor3;
-Servo myMotor4;
-
 BTD Btd(&Usb); 
 PS3BT PS3(&Btd); 
 
-bool printTemperature;
-bool printAngle;
+/*Servo myMotor;
+Servo myMotor2;
+Servo myMotor3;
+Servo myMotor4;*/
 
 int val;
 
+/*void setMotor(int val){
+  myMotor.writeMicroseconds(val);
+  myMotor2.writeMicroseconds(val);
+  myMotor3.writeMicroseconds(val);
+  myMotor4.writeMicroseconds(val);
+}*/
+
 void setup() {
-  myMotor.attach(3);
-  myMotor2.attach(5);
-  myMotor3.attach(6);
-  myMotor4.attach(10);
-
-  setMotor(1000);
-
   Serial.begin(115200);
-  #if !defined(__MIPSEL__)
-    while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
-  #endif
-  
+#if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
   if (Usb.Init() == -1) {
     Serial.print(F("\r\nOSC did not start"));
     while (1); //halt
   }
-    
   Serial.print(F("\r\nPS3 Bluetooth Library Started"));
 }
 
 void loop() {
   Usb.Task();
-  
-  if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
-    Serial.print("we are here");
     
-    // Analog button values can be read from almost all buttons
+  if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
     if (PS3.getAnalogButton(R2)) {  
-      //Serial.print(F("\r\nR2: "));
-      setMotor(1200+PS3.getAnalogButton(R2));
+      Serial.print(F("\r\nR2: "));
+      Serial.print(PS3.getAnalogButton(R2));
+    }
+
+    if (PS3.getAnalogButton(L2)) {
+      Serial.print(F("\r\nL2: "));
+      Serial.print(PS3.getAnalogButton(L2));
     }
     
     if (PS3.getButtonClick(PS)) {
@@ -62,11 +58,4 @@ void loop() {
       PS3.disconnect();
     }
   }
-}
-
-void setMotor(int val){
-  myMotor.writeMicroseconds(val);
-  myMotor2.writeMicroseconds(val);
-  myMotor3.writeMicroseconds(val);
-  myMotor4.writeMicroseconds(val);
 }
