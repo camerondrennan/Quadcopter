d23ba660e72ba6b9ae43fc0c712fafeff7f1cc9b
Cameron Deans <CDeansy@gmail.com>
2015-12-01 10:02:58 +0000

    Some basic interaction between the controller and motors

--- src/PS3BT/PS3BT.ino
+++ src/PS3BT/PS3BT.ino
@@ -13,22 +13,18 @@ USB Usb;
 BTD Btd(&Usb); 
 PS3BT PS3(&Btd); 
 
-/*Servo myMotor;
-Servo myMotor2;
-Servo myMotor3;
-Servo myMotor4;*/
+
+Servo motor;
+Servo motor2;
+Servo motor3;
+Servo motor4;
 
 int val;
 
-/*void setMotor(int val){
-  myMotor.writeMicroseconds(val);
-  myMotor2.writeMicroseconds(val);
-  myMotor3.writeMicroseconds(val);
-  myMotor4.writeMicroseconds(val);
-}*/
+
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 #if !defined(__MIPSEL__)
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
 #endif
@@ -37,20 +33,66 @@ void setup() {
     while (1); //halt
   }
   Serial.print(F("\r\nPS3 Bluetooth Library Started"));
+
+
+  motor.attach(5);
+  motor2.attach(3);
+  motor3.attach(7);
+  motor4.attach(9);
+
+  
+
+  motor.writeMicroseconds(1060);
+  motor2.writeMicroseconds(1060);
+  motor3.writeMicroseconds(1060);
+  motor4.writeMicroseconds(1060);
+
+  //delay(100);
+
+//  //myMotor.writeMicroseconds(1440);
+//  myMotor2.writeMicroseconds(1860);
+//  //myMotor3.writeMicroseconds(1440);
+//  //myMotor4.writeMicroseconds(1440);
+
+  
 }
+
 
 void loop() {
   Usb.Task();
+
+
+
+  int currentSpeed = 1400;
+
+  
+  //motor.writeMicroseconds(currentSpeed);
+  //motor2.writeMicroseconds(currentSpeed);
+  //motor3.writeMicroseconds(currentSpeed);
+  //motor4.writeMicroseconds(currentSpeed);
+  
     
   if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
     if (PS3.getAnalogButton(R2)) {  
       Serial.print(F("\r\nR2: "));
       Serial.print(PS3.getAnalogButton(R2));
+
+      //increaseMotorSpeed(currentSpeed);
+       motor.writeMicroseconds(1440);
+  motor2.writeMicroseconds(1440);
+  motor3.writeMicroseconds(1440);
+  motor4.writeMicroseconds(1440);
     }
 
     if (PS3.getAnalogButton(L2)) {
       Serial.print(F("\r\nL2: "));
       Serial.print(PS3.getAnalogButton(L2));
+
+      //decreaseMotorSpeed(currentSpeed);
+       motor.writeMicroseconds(1060);
+       motor2.writeMicroseconds(1060);
+        motor3.writeMicroseconds(1060);
+    motor4.writeMicroseconds(1060);
     }
     
     if (PS3.getButtonClick(PS)) {
