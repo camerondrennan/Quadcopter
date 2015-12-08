5500adff5fea6c78f95cadbd9949d20d520d6b76
Cameron Deans <CDeansy@gmail.com>
2015-12-08 10:54:18 +0000

    Pitch and roll implemented

--- src/PS3BT/PS3BT.ino
+++ src/PS3BT/PS3BT.ino
@@ -39,29 +39,31 @@ void setup() {
   Serial.print(F("\r\nPS3 Bluetooth Library Started"));
 
 
+  // Specify which pins the motors are attached to
   motor.attach(5);
   motor2.attach(7);
   motor3.attach(6);
   motor4.attach(3);
 
   
-  
+
+  // Give the motor speed variables a baseline value
   motor1Speed = 1100;
   motor2Speed = 1100;
   motor3Speed = 1100;
   motor4Speed = 1100;
 
   
-
+  // Initialise the motors
   motor.writeMicroseconds(1060);
   motor2.writeMicroseconds(1060);
   motor3.writeMicroseconds(1060);
   motor4.writeMicroseconds(1060);
 
-
-
   
 }
+
+// Writes the motor speed to the motors
 // 1% = 1127, 100% = 1860
 void writeMotorSpeed() {
   
@@ -75,12 +77,13 @@ void writeMotorSpeed() {
 void loop() {
   Usb.Task();
 
+  // Update the motor speed to the last value set
   writeMotorSpeed();
-
-
 
     
   if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
+    
+    // Increase throttle
     if (PS3.getAnalogButton(R2)) {  
       Serial.print(F("\r\nR2: "));
       Serial.print(PS3.getAnalogButton(R2));
@@ -94,6 +97,7 @@ void loop() {
         //writeMotorSpeed();
     }
 
+    // Decrease throttle
     if (PS3.getAnalogButton(L2)) {
       Serial.print(F("\r\nL2: "));
       Serial.print(PS3.getAnalogButton(L2));
@@ -105,13 +109,16 @@ void loop() {
         motor2Speed -= 5;
         motor3Speed -= 5;
         motor4Speed -= 5;
-        //writeMotorSpeed();
+        
     }
     
 
+    
     int yawAmount = 2;
+    // Left yaw
     if (PS3.getAnalogButton(L1)) {
       Serial.print(F("\r\nLeftYaw"));
+      
       // Increase the counter clockwise rotors by yawAmount
       // and decrease the clockwise rotors by yawAmount
       motor1Speed += yawAmount;
@@ -120,9 +127,11 @@ void loop() {
       motor3Speed -= yawAmount;
       motor4Speed -= yawAmount;      
     }
+    // Right yaw
     // Don't allow both L1 and R1 at the same time
     else if (PS3.getAnalogButton(R1)) {
       Serial.print(F("\r\nRightYaw"));
+      
       // Increase the clockwise rotors by yawAmount
       // and decrease the ccw rotors by yawAmount
       
@@ -133,41 +142,56 @@ void loop() {
       motor2Speed -= yawAmount;
     }
 
+    // Stop the motors
     if (PS3.getButtonClick(START)) {
         // Should kill the motors
         motor1Speed = 1120;
         motor2Speed = 1120;
         motor3Speed = 1120;
         motor4Speed = 1120;
-        //writeMotorSpeed();
+        
     }
-    
+
+    // Disconnect the controller
     if (PS3.getButtonClick(PS)) {
       Serial.print(F("\r\nPS"));
       PS3.disconnect();
     }
 
+    int pitchRollAmount = 5;
+
+    // Pitch forward
     if (PS3.getButtonClick(UP)) {
-      motor1Speed += 5;
+      // TODO: Add min max checks
+      motor1Speed -= pitchRollAmount;
+      motor2Speed += pitchRollAmount;
     }
 
+    // Pitch backwards
     if (PS3.getButtonClick(DOWN)) {
-      motor2Speed += 5;
+      // TODO: Add min max checks
+      motor1Speed += pitchRollAmount;
+      motor2Speed -= pitchRollAmount;
     }
 
+    // Roll left
     if (PS3.getButtonClick(LEFT)) {
-      motor3Speed += 5;
+      motor3Speed -= pitchRollAmount;
+      motor4Speed += pitchRollAmount;
     }
+
+    // Roll right
     if (PS3.getButtonClick(RIGHT)) {
-      motor4Speed += 5;
+      motor3Speed += pitchRollAmount;
+      motor4Speed -= pitchRollAmount;
     }
     
   }
   else {
-      // Fail safe if the controller disconnects?
-  motor1Speed = 1120;
-  motor2Speed = 1120;
-  motor3Speed = 1120;
-  motor4Speed = 1120;
+    // Fail safe if the controller disconnects?
+    motor1Speed = 1120;
+    motor2Speed = 1120;
+    motor3Speed = 1120;
+    motor4Speed = 1120;
   }
 }
